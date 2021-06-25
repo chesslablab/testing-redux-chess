291050f509b8aeeda1979e2909f2640a2e166c65
programarivm <info@programarivm.com>
2021-06-25 10:06:34 +0200

    Initialized server props

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Chess } from 'redux-chess';
 
 const props = {
-  host: '127.0.0.1',
-  port: '8080'
+  server: {
+    host: '127.0.0.1',
+    port: '8080'
+  }
 };
 
 ReactDOM.render(
